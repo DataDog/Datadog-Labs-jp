bbaf654961602d3c916ce95aefe7d0d6721a7c6b
kissyy <yoshihiro.kishida@datadog.com>
2022-06-29 18:07:19 +0900

    fix

--- terraform-101/Lab2-Monitor/metrics_monitor.tf
+++ terraform-101/Lab2-Monitor/metrics_monitor.tf
@@ -5,6 +5,10 @@ terraform {
     }
   }
 }
+
+variable "datadog_api_key" {}
+variable "datadog_app_key" {}
+
 provider "datadog" {
   api_key = "${var.datadog_api_key}"
   app_key = "${var.datadog_app_key}"
